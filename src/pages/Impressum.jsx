9803c4ba67ed69fae22bf49bdb249221ac21ca22
Luca <luca.gehlert2@gmail.com>
2024-08-22 22:09:58 +0200

    update

--- src/pages/Impressum.jsx
+++ src/pages/Impressum.jsx
@@ -7,13 +7,13 @@ export default function () {
     return (
         <>
             <h2 className="imp">Impressum</h2>
-             <h2 className="imp1">Angaben gemäß § 5 DDG</h2>
-             <h2 className="imp2">Mathias Wagner</h2>
-             <h2 className="imp2">Wird bald Hinzugefügt</h2>
-             <h2 className="imp2">Straße</h2>
-             <h2 className="imp2">Stadt</h2>
-             <h2 className="imp1">Kontakt</h2>
-             <h2 className="imp2">E-Mail: fragenova@gnymt.dev</h2>
+            <h2 className="imp1">Angaben gemäß § 5 DDG</h2>
+            <h2 className="imp2">Mathias Wagner</h2>
+            <h2 className="imp2">c/o COCENTER</h2>
+            <h2 className="imp2">Koppoldstr. 1</h2>
+            <h2 className="imp2">86551 Aichach</h2>
+            <h2 className="imp1">Kontakt</h2>
+            <h2 className="imp2">E-Mail: fragenova@gnymt.dev</h2>
         </>
     )
 }
